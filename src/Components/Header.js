d6495c0fe99d8a73e21b254e915b96184584fe55
Tagbo Chinedu <johntagbo2@gmail.com>
2022-08-11 22:23:08 +0100

    unfinished header2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+
 const Header = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [menu, setMenu] = useState(false);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    if (width > 767) {
+      setMenu(false);
+    }
+  }, [width]);
+
   return (
-    <div className="px-24 py-8">
-      <div className="flex justify-between mb-10">
+    <div className="px-6 xl:px-24 py-4 lg:py-8">
+      <div className="sm:flex justify-between mb-5 md:mb-10">
         <p className="text-sm text-slate-700">
           +34 931 591 821{" "}
           <a
@@ -11,7 +27,7 @@ const Header = () => {
             agency@inboundemotion.com
           </a>
         </p>
-        <ul className="flex">
+        <ul className="flex justify-end md:">
           <li className="mx-1">
             <a href="https://www.facebook.com/inboundemotion">
               <img
@@ -63,36 +79,77 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      <div className='mt-10 flex justify-between'>
+      <div className="md:mt-10 flex justify-between">
         <img
-          className="w-64"
+          className="w-48 h-4 md:w-64 md:h-6"
           src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1490281506-png/Inbound_Emotion/logo_nuevo.png"
           alt="logo"
         />
-        <ul className='flex'>
-          <li>
-            <a href="http://inboundemotion.com/inbound-marketing" className='text-slate-500 hover:text-slate-700 mx-4'>
-              Inbound Marketing
-            </a>
-          </li>
-          <li>
-            <a href="http://inboundemotion.com/diseno-web" className='text-slate-500 hover:text-slate-700 mx-4'>Web Design</a>
-          </li>
-          <li>
-            <a href="http://inboundemotion.com/blog" className='text-slate-500 hover:text-slate-700 mx-4'>Blog</a>
-          </li>
-          <li>
-            <a href="http://inboundemotion.com/recursos" className='text-slate-500 hover:text-slate-700 mx-4'>Means</a>
-          </li>
-          <li>
-            <a href="http://inboundemotion.com/agencia" className='text-slate-500 hover:text-slate-700 mx-4'>Agency</a>
-          </li>
-          <li>
-            <a href="https://www.inboundemotion.com/es/consulta-personalizada-de-inbound-marketing?hsLang=es" className='bg-orange-400 rounded-2xl text-white py-1.5 px-10 ml-4'>
-              REQUEST A CONSULTATION
-            </a>
-          </li>
-        </ul>
+
+        <div className="flex justify-between ">
+          <ul className={`${"lg:flex text-sm"} ${menu ? "" : "hidden"}`}>
+            <li className='mb-4 lg:mb-0 font-merri'>
+              <a
+                href="http://inboundemotion.com/inbound-marketing"
+                className="text-slate-500 hover:text-slate-700 lg:mx-4"
+              >
+                Inbound Marketing
+              </a>
+            </li>
+            <li className='my-4 lg:my-0 font-merri'>
+              <a
+                href="http://inboundemotion.com/diseno-web"
+                className="text-slate-500 hover:text-slate-700 lg:mx-4"
+              >
+                Web Design
+              </a>
+            </li>
+            <li className='my-4 lg:my-0 font-merri'>
+              <a
+                href="http://inboundemotion.com/blog"
+                className="text-slate-500 hover:text-slate-700 lg:mx-4"
+              >
+                Blog
+              </a>
+            </li>
+            <li className='my-4 lg:my-0 font-merri'>
+              <a
+                href="http://inboundemotion.com/recursos"
+                className="text-slate-500 hover:text-slate-700 lg:mx-4"
+              >
+                Means
+              </a>
+            </li>
+            <li className='my-4 lg:my-0 font-merri'>
+              <a
+                href="http://inboundemotion.com/agencia"
+                className="text-slate-500 hover:text-slate-700 lg:mx-4"
+              >
+                Agency
+              </a>
+            </li>
+            <li className='my-4 lg:my-0 font-advent'>
+              <a
+                href="https://www.inboundemotion.com/es/consulta-personalizada-de-inbound-marketing?hsLang=es"
+                className="bg-orange-400 rounded-2xl text-white py-1.5 px-10 lg:ml-4"
+              >
+                REQUEST A CONSULTATION
+              </a>
+            </li>
+          </ul>
+          <span
+            className="lg:hidden"
+            onClick={() => {
+              setMenu(!menu);
+            }}
+          >
+            {!menu ? (
+              <MenuIcon className="h-16 w-16 text-orange-400 lg:hidden" />
+            ) : (
+              <XIcon className="h-16 w-16 text-orange-400 lg:hidden" />
+            )}
+          </span>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'merri': "Merriweather Sans, serif",
+        'advent': "Advent Pro, sans-serif",
+        'julius': "Julius Sans One",
+        'alegreya' : 'Alegreya Sans, sans-serif'
+      },
+    },
   },
   plugins: [],
-}
+};
