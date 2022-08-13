a2bb618eb540d7dec482fbfa70450c6f0f003cd8
Tagbo Chinedu <johntagbo2@gmail.com>
2022-08-13 17:07:57 +0100

    finished footer

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -294,90 +294,166 @@ const Body = () => {
           </a>
         </div>
       </section>
-      <section>
-        <div>
-          <div>
-            <ul>
-              <li>
+      <section className="bg-footer text-white font-merri pt-24 pb-8 px-28">
+        <div className="flex justify-between">
+          <div className="w-6/12 flex justify-between">
+            <ul className="text-lg w-8/12">
+              <li className="mb-3.5 hover:underline">
                 <a href="http://inboundemotion.com/inbound-marketing">
                   WHAT IS INBOUND MARKETING?
                 </a>
               </li>
-              <li>
+              <li className="my-3.5 hover:underline">
                 <a href="http://inboundemotion.com/hubspot">
                   WHAT IS HUBSPOT AND HOW DOES IT WORK?
                 </a>
               </li>
-              <li>
+              <li className="my-3.5 hover:underline">
                 <a href="http://inboundemotion.com/diseno-web">
                   DESIGN YOUR WEBSITE
                 </a>
               </li>
-              <li>
+              <li className="my-3.5 hover:underline">
                 <a href="http://inboundemotion.com/recursos">WE HELP YOU</a>
               </li>
-              <li>
+              <li className="mt-3.5 hover:underline">
                 <a href="http://inboundemotion.com/agencia">KNOW US</a>
               </li>
             </ul>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <a href="https://plus.google.com/u/0/+Inboundemotionplus/posts">
+
+            <ul className="text-sm w-4/12">
+              <li className="mb-5 hover:underline">
+                <a
+                  href="https://plus.google.com/u/0/+Inboundemotionplus/posts"
+                  className="flex"
+                >
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1486351614-png/Inbound_Emotion/iconos/google.png"
                     alt="Google+"
-                  />Enter our Circle
+                  />
+                  Enter our Circle
                 </a>
               </li>
-              <li>
-                <a href="https://twitter.com/inboundemotion">
+              <li className="my-5 hover:underline">
+                <a href="https://twitter.com/inboundemotion" className="flex">
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1483587023-png/Inbound_Emotion/iconos/twiiter-1.png"
                     alt="Twitter"
-                  />follow us
+                  />
+                  follow us
                 </a>
               </li>
-              <li>
-                <a href="https://www.facebook.com/inboundemotion">
+              <li className="my-5 hover:underline">
+                <a
+                  href="https://www.facebook.com/inboundemotion"
+                  className="flex"
+                >
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1483587013-png/Inbound_Emotion/iconos/facebook.png"
                     alt=""
-                  />become a fan
+                  />
+                  become a fan
                 </a>
               </li>
-              <li>
-                <a href="http://www.pinterest.com/inboundemotion/">
+              <li className="my-5 hover:underline">
+                <a
+                  href="http://www.pinterest.com/inboundemotion/"
+                  className="flex"
+                >
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1483055534-png/Inbound_Emotion/iconos/pinterest.png"
                     alt=""
-                  />See our pins
+                  />
+                  See our pins
                 </a>
               </li>
-              <li>
-                <a href="https://www.linkedin.com/company/inbound-emotion">
+              <li className="my-5 hover:underline">
+                <a
+                  href="https://www.linkedin.com/company/inbound-emotion"
+                  className="flex"
+                >
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1486920267-png/Inbound_Emotion/iconos/linkedin.png"
                     alt=""
-                  />connect with us
+                  />
+                  connect with us
                 </a>
               </li>
-              <li>
-                <a href="http://inboundemotion.com/blog/rss.xml">
+              <li className="mt-5 hover:underline">
+                <a
+                  href="http://inboundemotion.com/blog/rss.xml"
+                  className="flex"
+                >
                   <img
+                    className="w-6 h-6 mr-4"
                     src="https://www.inboundemotion.com/hs-fs/hub/340557/file-1483587018-png/Inbound_Emotion/iconos/rss.png"
                     alt=""
-                  />Don't miss a thing
+                  />
+                  Don't miss a thing
                 </a>
               </li>
             </ul>
           </div>
-          <div>
-            <div></div>
-            <div></div>
+          <div className="w-5/12">
+            <div className="mb-10">
+              <h4 className="text-lg">WHERE WE ARE</h4>
+              <p className="leading-6 text-sm">
+                Inbound Emotion Barcelona
+                <br />
+                C/ Rambla de Catalunya 125, 3º 2ª
+                <br />
+                08008 Barcelona
+                <br />
+                +34 931 591 821
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm w-5/12 leading-6">
+                Inbound Emotion Mallorca
+                <br />
+                Coworking Sa Batedora
+                <br />
+                C/ Son Sabater, 35
+                <br />
+                07510 Sineu
+                <br />
+                +34 699 480 918
+              </p>
+              <p className="text-sm w-5/12 leading-6">
+                Inbound Emotion Tarragona <br />
+                C/ Monterols 16, 1º 2ª <br />
+                43201 Reus <br />
+                +34 977 894 264 <br />
+              </p>
+            </div>
           </div>
-          <div></div>
+        </div>{" "}
+        <div className='mt-16'>
+          <h2 className="text-2xl text-orng leading-9">
+            At Inbound Emotion,
+            <br /> we do the marketing that people like
+          </h2>
+
+          <ul className="flex justify-between mt-6">
+            <li className="hover:underline text-sm w-3/12">
+              <a href="https://www.inboundemotion.com/politica-privacidad?hsLang=es">
+                Legal Notice and Privacy Policy
+              </a>
+            </li>
+            <li className="hover:underline text-sm w-3/12">
+              <a href="https://www.inboundemotion.com/politica-cookies/?hsLang=es">
+                Cookies policy
+              </a>
+            </li>
+            <li className="hover:underline text-sm w-4/12">
+              <a href="mailto: agencia@inboundemotion.com">agency@inboundemotion.com</a>
+            </li>
+          </ul>
         </div>
       </section>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'merri': "Merriweather Sans, serif",
-        'advent': "Advent Pro, sans-serif",
-        'julius': "Julius Sans One",
-        'alegreya' : 'Alegreya Sans, sans-serif',
-        'source' : 'Source Serif Pro',
-        'merr' : 'Merriweather'
+        "merri": "Merriweather Sans, serif",
+        "advent": "Advent Pro, sans-serif",
+        "julius": "Julius Sans One",
+        "alegreya": "Alegreya Sans, sans-serif",
+        "source": "Source Serif Pro",
+        "merr": "Merriweather",
       },
       colors: {
-        'bgBody': '#FFF9F4',
-        'orng' : '#EC8128',
-        'bgBody2' : '#75A6A2',
-        ''
+        "bgBody": "#FFF9F4",
+        "orng": "#EC8128",
+        "bgBody2": "#75A6A2",
+        "footer": "#494949",
       },
     },
   },
